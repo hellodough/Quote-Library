70807f40bbf057748fdfe7161cb5ddb132b9282f
Anna <doannado@gmail.com>
2023-05-06 22:10:49 -0500

    test: setup test and babel

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import HomePage from './index';
+import HomePage from '.';
 import { screen } from '@testing-library/react';
 
 // const mockData = [
